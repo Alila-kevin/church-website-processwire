3e7f1398f515186ac313bbdbec15ded3518bcee5
Kevin Alila <85939629+Alila-kevin@users.noreply.github.com>
2023-05-30 12:24:04 +0300

    Update index.php
    still working

--- site/assets/index.php
+++ site/assets/index.php
@@ -2,3 +2,4 @@
 // Intentionally left blank to test that htaccess rewrite rules are working. 
 // Accessing this file from http should produce a '403 forbidden' error,
 // since all PHP files are blocked under /assets/.
+//nathing here
